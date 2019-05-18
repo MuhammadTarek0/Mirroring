984c1a6ccda2e277f5f54022d62d447dcf34eef5
Muhammad Tarek <45735852+MuhammadTarek0@users.noreply.github.com>
2019-05-18 11:05:03 +0200

    Add files via upload

---
+++ Point.js
@@ -0,0 +1,25 @@
+﻿function Point(x, y) {
+
+    this.x = x;
+    this.y = y;
+    var hamada = 3;
+    var that = this;
+    this.Draw = function (ctx) {
+        ///<param name="ctx" type="CanvasRenderingContext2D"/>
+        ctx.beginPath()
+        ctx.arc(that.x, that.y, 10, 0, 3.14159268 * 2);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    this.IsMouseOver = function (x, y) {
+        var dy = y - that.y;
+        var dx = x - that.x;
+        var d = Math.sqrt(dx * dx + dy * dy);
+        if (d < 10) {
+            return true;
+        }
+        return false;
+    }
+}
+
